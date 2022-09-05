f36357d82b1025a22e05fa28f39d9394a4b0f7a5
macoto5636 <maakitak@icloud.com>
2022-09-06 01:31:32 +0900

    footerの見た目変更

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,28 +1,6 @@
 <template>
-  <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8">
-    <div class="sm:flex sm:items-center sm:justify-between">
-      <a href="#" class="flex items-center mb-4 sm:mb-0">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap">KITANO's Portfolio</span>
-      </a>
-      <ul class="flex flex-wrap items-center mb-6 text-sm text-sky-500 sm:mb-0">
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">About me</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">Carrer</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">Skills</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">Works</a>
-        </li>
-        <li>
-          <a href="#" class="hover:underline">Contact</a>
-        </li>
-      </ul>
-    </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-    <span class="block text-sm text-gray-500 sm:text-center">©️2022 My Portfolio All Rights Reserved.</span>
+  <footer class="pb-8 bg-white">
+    <hr class="mb-6 border-gray-200 sm:mx-auto" />
+    <span class="block text-sm text-gray-500 sm:text-center">©️2022 KITANO's Portfolio All Rights Reserved.</span>
   </footer>
 </template>
